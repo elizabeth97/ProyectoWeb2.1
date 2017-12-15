4b5bac1ac583189de218f379766b97aa01b2a3eb
sistemas_contact506 <sistemas@contact506.com>
2017-12-15 16:03:16 -0600

    contactenos agregado

--- js/util.js
+++ js/util.js
@@ -34,6 +34,19 @@
 
 	};
 
+	$(document).ready(function(){
+	var altura = $('.menu').offset().top;
+
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu').addClass('menu-fixed');
+		} else {
+			$('.menu').removeClass('menu-fixed');
+		}
+	});
+
+});
+
 	/**
 	 * Panel-ify an element.
 	 * @param {object} userConfig User config.
